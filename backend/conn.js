f8bfa5ea9b1a57894ab7295142e647cbccf399f9
Shivani Saurabh <saurabh.shivani3@gmail.com>
2026-05-29 16:01:13 +0530

    Sprint 1 completed

--- backend/conn.js
+++ backend/conn.js
@@ -2,6 +2,14 @@ const mongoose = require('mongoose')
 const URL = process.env.MONGO_URI
 
 mongoose.connect(URL)
+  .then(() => {
+    console.log("MongoDB Connected")
+  })
+  .catch(err => {
+    console.log("ERROR NAME:", err.name)
+    console.log("ERROR MESSAGE:", err.message)
+    console.log("FULL ERROR:", err)
+  })
 mongoose.Promise = global.Promise
 
 const db = mongoose.connection
